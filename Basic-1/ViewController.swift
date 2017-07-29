f0505d848514435e52e6d37020a49a720b92509f
Eder Rengifo <ederrengifo@Eders-MacBook-Pro.local>
2017-07-28 23:03:00 -0500

    updating

--- Basic-1/ViewController.swift
+++ Basic-1/ViewController.swift
@@ -9,23 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 
     @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)"
+        
         let alert = UIAlertController(title:"Hello world",
-                                      message: "This is my first alert",
+                                      message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title:"Close",
+        let action = UIAlertAction(title:"OK",
                                    style: .default,
                                    handler: nil)
         
