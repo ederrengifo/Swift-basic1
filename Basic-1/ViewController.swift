40ec6ad2bd359878fbbb41003c3de8bb063ceac0
Eder Rengifo <ederrengifo@Eders-MacBook-Pro.local>
2017-07-28 22:09:41 -0500

    Initial Commit

---
+++ Basic-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic-1
+//
+//  Created by Eder Rengifo on 7/28/17.
+//  Copyright © 2017 Eder Rengifo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
