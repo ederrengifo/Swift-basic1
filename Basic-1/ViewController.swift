e7d220ea0d7583c6ac957eab7c85deaa9a81209d
Eder Rengifo <ederrengifo@Eders-MacBook-Pro.local>
2017-07-29 23:05:08 -0500

    solving chore

--- Basic-1/ViewController.swift
+++ Basic-1/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 0
     var targetValue: Int = 0
-    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,12 +46,17 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
         
         startNewRound()
+        updateLabels()
     }
     
     func startNewRound() {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = lroundf(slider.value)
         slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
     
 }
